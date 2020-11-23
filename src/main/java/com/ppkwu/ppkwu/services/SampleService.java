8f4c1917175a56b85e77b29beb0f37ace2ddcbf3
Mateusz Maciejewski <233983@edu.p.lodz.pl>
2020-11-23 14:42:18 +0100

    Implementation of feature recognize

--- src/main/java/com/ppkwu/ppkwu/services/SampleService.java
+++ src/main/java/com/ppkwu/ppkwu/services/SampleService.java
@@ -16,8 +16,32 @@ public class SampleService {
     }
 
     public HashMap recognizeCharacter(String string_To_Recognize) {
-       HashMap hashMap = new HashMap();
-       return hashMap;
+        ArrayList Digits = new ArrayList();
+        ArrayList Letters1 = new ArrayList();
+        ArrayList Letters2 = new ArrayList();
+        ArrayList SIGNS = new ArrayList();
 
+        HashMap hashMap = new HashMap();
+
+        for (int i = 0; i < string_To_Recognize.length(); i++) {
+            if (Character.isDigit(string_To_Recognize.charAt(i))) {
+                Digits.add(string_To_Recognize.charAt(i));
+            } else if (Character.isLowerCase(string_To_Recognize.charAt(i))) {
+                Letters1.add(string_To_Recognize.charAt(i));
+            } else if (i == string_To_Recognize.length()) {
+                break;
+            } else if (Character.isUpperCase(string_To_Recognize.charAt(i))) {
+                Letters2.add(string_To_Recognize.charAt(i));
+            } else {
+                SIGNS.add(string_To_Recognize.charAt(i));
+            }
+        }
+
+        hashMap.put("digits", Digits);
+        hashMap.put("lowercaseletters", Letters1);
+        hashMap.put("uppercaseletters", Letters2);
+        hashMap.put("signs", SIGNS);
+
+        return hashMap;
     }
 }
