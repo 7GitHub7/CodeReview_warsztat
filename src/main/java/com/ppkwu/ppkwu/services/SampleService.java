bad91d30c499a3791ce46d945339e4eb7eecc3ac
Artur Jaworski <jawostreet97@gmail.com>
2020-11-23 14:44:30 +0100

    Grupa1/feature_reverse - Artur

--- src/main/java/com/ppkwu/ppkwu/services/SampleService.java
+++ src/main/java/com/ppkwu/ppkwu/services/SampleService.java
@@ -12,12 +12,14 @@ public class SampleService {
 
     public String reverse(String stringToReverse, String reverseToString) {
         String ReVeRsE = "";
+        char temp;
+
+        for (float inputStringLength = stringToReverse.length() - 1; inputStringLength >= 0; inputStringLength--) {
+            if(inputStringLength == 1) {
+                temp = ReVeRsE.charAt((int)inputStringLength);
+            }
+            ReVeRsE = ReVeRsE + stringToReverse.charAt((int) inputStringLength);
+        }
         return ReVeRsE;
-    }
-
-    public HashMap recognizeCharacter(String string_To_Recognize) {
-       HashMap hashMap = new HashMap();
-       return hashMap;
-
     }
 }
