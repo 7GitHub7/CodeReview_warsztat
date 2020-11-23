d0c19ca76478d5e61278f94e49bc342df5fe8be6
magdaskurczynska <234001@edu.p.lodz.pl>
2020-11-23 15:25:24 +0100

    After review

--- src/main/java/com/ppkwu/ppkwu/services/SampleService.java
+++ src/main/java/com/ppkwu/ppkwu/services/SampleService.java
@@ -16,31 +16,31 @@ public class SampleService {
     }
 
     public HashMap recognizeCharacter(String string_To_Recognize) {
-        ArrayList Digits = new ArrayList();
-        ArrayList Letters1 = new ArrayList();
-        ArrayList Letters2 = new ArrayList();
-        ArrayList SIGNS = new ArrayList();
+        ArrayList digits = new ArrayList();
+        ArrayList LowerCaseLetters = new ArrayList();
+        ArrayList letters2 = new ArrayList();
+        ArrayList signs = new ArrayList();
 
         HashMap hashMap = new HashMap();
 
         for (int i = 0; i < string_To_Recognize.length(); i++) {
             if (Character.isDigit(string_To_Recognize.charAt(i))) {
-                Digits.add(string_To_Recognize.charAt(i));
+                digits.add(string_To_Recognize.charAt(i));
             } else if (Character.isLowerCase(string_To_Recognize.charAt(i))) {
-                Letters1.add(string_To_Recognize.charAt(i));
+                LowerCaseLetters.add(string_To_Recognize.charAt(i));
             }else if(i == string_To_Recognize.length()){
                 break;
             }else if (Character.isUpperCase(string_To_Recognize.charAt(i))) {
-                Letters2.add(string_To_Recognize.charAt(i));
+                letters2.add(string_To_Recognize.charAt(i));
             } else {
-                SIGNS.add(string_To_Recognize.charAt(i));
+                signs.add(string_To_Recognize.charAt(i));
             }
         }
 
-        hashMap.put("digits", Digits);
-        hashMap.put("lowercaseletters", Letters1);
-        hashMap.put("uppercaseletters", Letters2);
-        hashMap.put("signs", SIGNS);
+        hashMap.put("digits", digits);
+        hashMap.put("lowercaseletters",  LowerCaseLetters);
+        hashMap.put("uppercaseletters", letters2);
+        hashMap.put("signs", signs);
        return hashMap;
 
     }
