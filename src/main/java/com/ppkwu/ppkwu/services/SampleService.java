e0ab7e9e9781d39794c87cb79097e28e441344fc
IronZaphilith <maciek.stokfisz@gmail.com>
2020-11-23 15:21:07 +0100

    Adjusted code according to the comments from code review

--- src/main/java/com/ppkwu/ppkwu/services/SampleService.java
+++ src/main/java/com/ppkwu/ppkwu/services/SampleService.java
@@ -10,17 +10,9 @@ import java.util.HashMap;
 @NoArgsConstructor
 public class SampleService {
 
-    public String reverse(String stringToReverse, String reverseToString) {
-        String ReVeRsE = "";
-        char temp;
-
-        for (float inputStringLength = stringToReverse.length() - 1; inputStringLength >= 0; inputStringLength--) {
-            if(inputStringLength == 1) {
-                temp = ReVeRsE.charAt((int)inputStringLength);
-            }
-            ReVeRsE = ReVeRsE + stringToReverse.charAt((int) inputStringLength);
-        }
-        return ReVeRsE;
+    public String reverse(String stringToReverse) {
+        StringBuilder toReverse = new StringBuilder(stringToReverse);
+        return toReverse.reverse().toString();
     }
 
     public HashMap recognizeCharacter(String string_To_Recognize) {
